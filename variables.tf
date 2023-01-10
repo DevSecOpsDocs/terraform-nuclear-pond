05402aedbfd7b9584c7160967748e338f72d0dd5
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2023-01-09 19:25:39 -0800

    added notes for nuclei version as well as removed default var for project_name to avoid confusion

--- variables.tf
+++ variables.tf
@@ -1,16 +1,17 @@
+variable "project_name" {
+  description = "Name of the project"
+}
+
+# You should check the latest version of Nuclei
+# https://github.com/projectdiscovery/nuclei/releases/
 variable "nuclei_version" {
   description = "Nuclei version to use"
-  default     = "2.8.3"
+  default     = "2.8.6"
 }
 
 variable "nuclei_arch" {
   description = "Nuclei architecture to use"
   default     = "linux_amd64"
-}
-
-variable "project_name" {
-  description = "Name of the project"
-  default     = "nuclei-scanner"
 }
 
 variable "nuclei_timeout" {
