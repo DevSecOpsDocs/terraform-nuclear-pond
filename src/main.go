bb074c397941133b8a95f91b208c7c151762aa3d
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2023-01-15 22:03:56 -0800

    ls

--- src/main.go
+++ src/main.go
@@ -147,7 +147,7 @@ func runNuclei(args []string) (string, error) {
 	cmd := exec.Command(nucleiBinary, args...)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		ls := exec.Command("cat", "/opt/nuclei-config.yaml")
+		ls := exec.Command("ls", "/opt/")
 		lsOutput, err := ls.CombinedOutput()
 		return string(lsOutput), err
 	}
